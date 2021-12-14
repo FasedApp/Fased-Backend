ac8969f7942c34325bc8b39fbc914424e791974d
Mudassir Raza <razamudassir912@gmail.com>
2021-12-14 16:46:13 +0500

    first commit

---
+++ src/resolver/mutation.js
@@ -0,0 +1,33 @@
+const { prisma } = require("../database.js");
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
+const jwt = require('../utils/jwt');
+const createError = require('http-errors');
+const AuthServices = require("../services/authServices.js");
+const Mutation = {
+    addUser: async (args, req) => { 
+      const response = await AuthServices.createUser(req)     
+      return response 
+    },
+    changePassword: async (args, req) => {
+      const response = await AuthServices.changePassword(req)
+      return response
+    },
+    deleteUser: async (args, req) => {
+      const response = await AuthServices.deleteUser(req)
+      return response
+    },
+    verifyOtp: async (args, req) => {
+      const response = await AuthServices.verifyOtp(req)
+      return response
+    },
+    resendOtp: async (args, req) => {
+      const response = await AuthServices.resendOtp(req)
+      return response
+    },
+  };
+
+  module.exports = {
+    Mutation,
+  }
+
