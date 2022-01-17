55a3a433c989476e0a77939cce01f9d956b69e11
Mudassir Raza <mudassir.raza@techstirr.com>
2022-01-17 20:28:44 +0500

    asd

---
+++ prisma/migrations/20220117152759_cascade_in_report/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "Report" DROP CONSTRAINT "Report_CategoryId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Report" ADD CONSTRAINT "Report_CategoryId_fkey" FOREIGN KEY ("CategoryId") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
